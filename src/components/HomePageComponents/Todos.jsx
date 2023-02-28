87d4f20bf61c66e1302524336ab15c64a8e4d0ad
Drago78624 <maazahmed78624@gmail.com>
2023-02-28 09:21:59 +0500

    fixed noob bug

--- src/components/HomePageComponents/Todos.jsx
+++ src/components/HomePageComponents/Todos.jsx
@@ -13,7 +13,7 @@ import AuthContext from "../../auth-context";
 import Form from "./Form";
 import UtilityContext from "../../utility-context";
 import MessageContext from "../../message-context";
-
+import { db } from "../../firebase-config";
 const Todos = () => {
   const utilityCtx = useContext(UtilityContext);
   const msgCtx = useContext(MessageContext);
